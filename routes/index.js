670db5b3886e0b91908c04f9116c5f3f9753fdde
mahesh1m <mahesh1m.mys@gmail.com>
2025-11-14 11:52:41 +0530

    Add hobbies route to return AI-related hobby information

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.send('API Home');
 });
 
+router.get('/hobbies', (req, res) => {
+  res.send('learning and working with ai tools');
+});
+
 module.exports = router;
